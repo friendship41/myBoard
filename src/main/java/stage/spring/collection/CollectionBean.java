019f241603642d30019100140600eca45c40f9ca
ChangHo Choi <stage41@nate.com>
2020-01-17 13:20:38 +0900

    Collecting들 의존성 주입하는 방식

--- src/main/java/stage/spring/collection/CollectionBean.java
+++ src/main/java/stage/spring/collection/CollectionBean.java
@@ -1,9 +1,15 @@
 package stage.spring.collection;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
 public class CollectionBean {
 	private List<String> addressList;
+	private Map<String, String> addressMap;
+	private Properties addressProperties;
+	private Set<String> addressSet;
 	
 	public List<String> getAddressList() {
 		return addressList;
@@ -12,6 +18,32 @@ public class CollectionBean {
 	public void setAddressList(List<String> addressList) {
 		this.addressList = addressList;
 	}
+
+	public Map<String, String> getAddressMap() {
+		return addressMap;
+	}
+
+	public void setAddressMap(Map<String, String> addressMap) {
+		this.addressMap = addressMap;
+	}
+
+	public Properties getAddressProperties() {
+		return addressProperties;
+	}
+
+	public void setAddressProperties(Properties addressProperties) {
+		this.addressProperties = addressProperties;
+	}
+
+	public Set<String> getAddressSet() {
+		return addressSet;
+	}
+
+	public void setAddressSet(Set<String> addressSet) {
+		this.addressSet = addressSet;
+	}
+	
+	
 	
 	
 }
--- src/main/java/stage/spring/collection/CollectionBeanClient.java
+++ src/main/java/stage/spring/collection/CollectionBeanClient.java
@@ -1,6 +1,9 @@
 package stage.spring.collection;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -13,8 +16,28 @@ public class CollectionBeanClient {
 		CollectionBean bean = factory.getBean(CollectionBean.class);
 		
 		List<String> addressList = bean.getAddressList();
-		
 		System.out.println(addressList);
+		
+		System.out.println("============================");
+		Map<String, String> addressMap = bean.getAddressMap();
+		for(String addressKey : addressMap.keySet())
+		{
+			System.out.println(String.format("키 : %s, 값: %s", addressKey, addressMap.get(addressKey)));
+		}
+		
+		System.out.println("============================");
+		Properties addressProperties = bean.getAddressProperties();
+		for(String addressKey : addressProperties.stringPropertyNames())
+		{
+			System.out.println(String.format("키 : %s, 값: %s", addressKey, addressProperties.get(addressKey)));
+		}
+		
+		System.out.println("============================");
+		Set<String> addressSet = bean.getAddressSet();
+		for(String address : addressSet)
+		{
+			System.out.println(address);
+		}
 		
 		factory.close();
 	}
