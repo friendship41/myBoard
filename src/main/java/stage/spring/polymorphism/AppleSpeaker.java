0ce6b9356019610a84c5542ec27cdaac7fdb7297
ChangHo Choi <stage41@nate.com>
2020-01-17 14:04:46 +0900

    Qualifier를 이용한 특정빈 선택

--- src/main/java/stage/spring/polymorphism/AppleSpeaker.java
+++ src/main/java/stage/spring/polymorphism/AppleSpeaker.java
@@ -1,5 +1,8 @@
 package stage.spring.polymorphism;
 
+import org.springframework.stereotype.Component;
+
+@Component("apple")
 public class AppleSpeaker implements Speaker
 {
 	public AppleSpeaker() {
--- src/main/java/stage/spring/polymorphism/LgTV.java
+++ src/main/java/stage/spring/polymorphism/LgTV.java
@@ -1,12 +1,14 @@
 package stage.spring.polymorphism;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LgTV implements TV {
 	
 	@Autowired
+	@Qualifier("apple")
 	private Speaker speaker;
 	
 	public LgTV() {
