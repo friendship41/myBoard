ee8ab2749f27522d44ab7c5a3e20be69eb58b3b1
ChangHo Choi <stage41@nate.com>
2020-01-17 09:53:11 +0900

    applicationContext를 이용한 팩토리 실험

--- src/main/java/stage/spring/polymorphism/TVUser.java
+++ src/main/java/stage/spring/polymorphism/TVUser.java
@@ -1,16 +1,32 @@
 package stage.spring.polymorphism;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 class TVUser {
 
 	public static void main(String[] args) {
+//		높은 결합도의 구조
 //		TV tv = new SamsungTV(); 
 //		TV tv = new LgTV();
-		BeanFactory factory = new BeanFactory();
-		TV tv = (TV) factory.getBean(args[0]);
+		
+//		조금 나아진 결합도의 구조
+//		BeanFactory factory = new BeanFactory();
+//		TV tv = (TV) factory.getBean(args[0]);
+		
+//		스프링 기능을 이용한 낮은 결합도 구조
+		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
+		
+		TV tv = factory.getBean(SamsungTV.class);
+		System.out.println(tv.getClass().getName());
+		
 		tv.powerOn();
 		tv.volumeUp();
 		tv.volumeDown();
 		tv.powerOff();
+		
+		factory.close();
 	}
 
 }
